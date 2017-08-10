93b63819071c9c087750b767d8151a3f13dc3497
CC305906 <CC305906@L0610017655.africa.nedcor.net>
2017-08-10 10:42:47 +0200

    test

---
+++ src/ObjectReposiroty.java
@@ -0,0 +1,31 @@
+
+
+
+
+public class ObjectReposiroty {
+
+public static String wikiauto_loginbuton_xid = "//html/body/div[4]/div[1]/div[1]/ul/li[5]/a";
+public static String wikiauto_username_id ="wpName1";
+public static String wikiauto_password_name = "wpPassword";
+public static String wikiauto_remember_id ="wpRemember";
+public static String wikiauto_loginbutton_xpath = "//html/body/div[3]/div[4]/div[3]/div/div[2]/form/div/div[4]/div/button";
+public static String wikiauto_error_msg_xpath = "//html/body/div[3]/div[4]/div[3]/div/div[2]/form/div[1]";
+public static String wikiauto_newlogin_xpath ="//html/body/div[3]/div[4]/div[3]/div/div[2]/form/div[2]/div[5]/div/button";
+public static String wikiauto_secruitycheck_tag = "CAPTCHA Security check";
+
+
+// declaring objects for tourssite
+public static String  newtours_login_username_name = "userName";
+public static String  newtours_login_password_name = "password";
+public static String  newtours_login_sign_xpath = "//html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[4]/td[2]/div/input";
+public static String  newtours_reigsterlink_xpath ="//html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/p/font/a";
+//------ registration details on toursite
+public static String  newtours_register_first_name ="firstName";
+public static String  newtours_register_last_name="lastName";
+public static String  newtours_register_phone_name ="phone";
+public static String  newtours_register_emai_id ="userName";
+//------- input for excel spread sheet
+public static String xlpath = "C:\\Users\\cc305906\\Documents\\Selinium\\Inputs\\Input_Data2.xls";
+
+
+}
