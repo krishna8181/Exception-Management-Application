6097b3bc16daf29ca254b6a18a6692f33ef1874b
CC305906 <CC305906@L0610017655.africa.nedcor.net>
2017-08-10 10:42:26 +0200

    test

---
+++ src/QuickWork.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class QuickWork {
+	
+	public static WebDriver driver;
+
+public static void main(String[] args) {
+	System.setProperty("webdriver.gecko.driver", "C:\\Users\\cc305906\\Documents\\Selinium\\geckodriver.exe");
+	driver = new FirefoxDriver();
+	
+	driver.get("http://www.rediff.com/");
+
+	}
+
+}
